3b0ed900835ab816e029df983fa815c3aaf02c3b
ptanX <nguyenthaibinhbk@gmail.com>
2019-05-08 23:43:08 +0700

    update java oca chapter 3

---
+++ java_oca/src/chapter3/Phone.java
@@ -0,0 +1,9 @@
+package chapter3;
+
+public class Phone {
+	String phoneNumber = "123456789";
+	void setPhoneNumber() {
+		String phoneNumber;
+		phoneNumber = "987654321";
+	}
+}
---
+++ java_oca/src/chapter3/TestPhone.java
@@ -0,0 +1,10 @@
+package chapter3;
+
+public class TestPhone {
+	public static void main(String... args) {
+		Phone p1 = new Phone();
+		p1.setPhoneNumber();
+		System.out.println(p1.phoneNumber);
+	}
+
+}
